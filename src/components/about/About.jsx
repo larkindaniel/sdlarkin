df9d27a1efdf3649ddcaefe74cfeac11dc94bfcd
slarkin <slarkin@blackberry.com>
2023-07-10 14:10:54 -0400

    polished hero

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,11 +1,44 @@
 import React from "react";
 import "./about.css";
+import ME from '../../assets/R.jpg'
+// import {FaAward, FiUsers} from 'react-icons/fa'
+// import {VscFolderLibrary} from 'react-icons/vsc'
+
 
 const About = () => {
   return ( 
     
-  <section id="about">'
-    <a href ="#">test</a>About
+  <section id="about">
+   <h5>About Me</h5>
+   <div className="container about__container">
+   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid, quisquam? Voluptatibus dolores expedita accusamus molestiae eligendi ea, consequuntur modi non repudiandae itaque officia repellat ex placeat error reprehenderit, at aspernatur?</p>
+      <div className="about__content">
+        <div className="about__cards">
+          <article className = "about__card">
+            <h3>Experience</h3>
+            <small>3+ years Working</small>
+          </article>
+
+          <article className = "about__card">
+         
+            <h3>Clients</h3>
+            <small>200+ Worldwide</small>
+          </article>
+
+          <article className = "about__card">
+        
+            <h3>Projects</h3>
+            <small>80+ Completed</small>
+          </article>
+        </div>
+        <div className="about__me">
+        <div className="about__me-image">
+          <img src ={ME} alt ="ME"/>
+        </div>
+      </div>
+        
+      </div>
+   </div>
   </section>
   );
 };
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,9 +6,9 @@ const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h className="header__intro">hi, my name is </h>
-        <h2>Daniel Larkin.</h2>
-        <h2 className="text-light">I'm a Software Engineer.</h2>
+        <p className="header__intro">hi, my name is </p>
+        <h2>Daniel Larkin</h2>
+        <h2 class="text-light secondary__header">I'm a Software Engineer.</h2>
         <HeaderSocials/>
       </div>
     </header>
