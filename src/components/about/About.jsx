1f25ef8bf66e0190ed2b1e1aabb34366db7e775c
slarkin <slarkin@blackberry.com>
2023-07-10 19:49:30 -0400

    time to eat food

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,45 +1,40 @@
 import React from "react";
 import "./about.css";
-import ME from '../../assets/R.jpg'
+import ME from "../../assets/headshot.jpg";
 // import {FaAward, FiUsers} from 'react-icons/fa'
 // import {VscFolderLibrary} from 'react-icons/vsc'
 
-
 const About = () => {
-  return ( 
-    
-  <section id="about">
-   <h5>About Me</h5>
-   <div className="container about__container">
-   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid, quisquam? Voluptatibus dolores expedita accusamus molestiae eligendi ea, consequuntur modi non repudiandae itaque officia repellat ex placeat error reprehenderit, at aspernatur?</p>
-      <div className="about__content">
-        <div className="about__cards">
-          <article className = "about__card">
-            <h3>Experience</h3>
-            <small>3+ years Working</small>
-          </article>
+  return (
+    <section id="about">
+      <div className="container container__header"><h5>About Me</h5><hr></hr></div>
+      
+     
 
-          <article className = "about__card">
-         
-            <h3>Clients</h3>
-            <small>200+ Worldwide</small>
-          </article>
-
-          <article className = "about__card">
+      <div className="container about__container">
+      
         
-            <h3>Projects</h3>
-            <small>80+ Completed</small>
-          </article>
+        <div className="about__content">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid,
+            quisquam? Voluptatibus dolores expedita accusamus molestiae eligendi
+            ea, consequuntur modi non repudiandae itaque officia repellat ex
+            placeat error reprehenderit, at aspernatur?
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid,
+            quisquam? Voluptatibus dolores expedita accusamus molestiae eligendi
+            ea, consequuntur modi non repudiandae itaque officia repellat ex
+            placeat error reprehenderit, at aspernatur?
+          </p>
         </div>
         <div className="about__me">
-        <div className="about__me-image">
-          <img src ={ME} alt ="ME"/>
+          <div className="about__me-image">
+            <img src={ME} alt="ME" />
+          </div>
         </div>
       </div>
-        
-      </div>
-   </div>
-  </section>
+    </section>
   );
 };
 
