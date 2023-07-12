1fadc6f6b8c6dfb515563de4a1f496d2d4bba8d4
slarkin <slarkin@blackberry.com>
2023-07-11 22:04:31 -0400

    whyamistillatwork

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,7 +2,21 @@ import React from "react";
 import "./contact.css";
 
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  return (
+    <section id="contact">
+      <div className="container container__header">
+        <h5>Let's Get in Touch</h5>
+      </div>
+      <div className="container container__contact">
+        <form action = "">
+            <input type="text" name = 'name' paceholder="Your Name" required/>
+            <input type="email" name = 'email' paceholder="Your Email" required/>
+            <textarea name="message" rows="7" placeholder="Your Message"></textarea>
+            <button type = "submit">Send Message</button>
+          </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
